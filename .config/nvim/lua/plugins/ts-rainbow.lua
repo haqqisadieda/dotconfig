c9ceb6d5254c6614b3a3be1c4bc048d3c9a1aa5d
haqqisadieda <haqqisadiedapandega@gmail.com>
2023-04-29 18:06:14 +0700

    adding tailwindcss config on nvim

--- .config/nvim/lua/plugins/ts-rainbow.lua
+++ .config/nvim/lua/plugins/ts-rainbow.lua
@@ -1,5 +1,14 @@
 return {
-  "p00f/nvim-ts-rainbow",
-  event = "BufRead",
-  dependencies = "nvim-treesitter/nvim-treesitter",
+  {
+    "p00f/nvim-ts-rainbow",
+    event = "BufRead",
+    dependencies = "nvim-treesitter/nvim-treesitter",
+  },
+  {
+    "windwp/nvim-ts-autotag",
+    event = "VeryLazy",
+    init = function()
+      require("nvim-ts-autotag").setup()
+    end,
+  },
 }
